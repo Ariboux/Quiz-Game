24403ab120c50a6bdd0abb0707a79148d2fb8619
AD <95749329+Ariboux@users.noreply.github.com>
2024-02-04 18:32:32 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/env python
+ from distutils.core import setup
+ setup(
+ name='Quiz_Game-Balderacchi_Adrien-CDOF1',
+ version='1.0',
+ author='Ariboux',
+ license='MIT',
+ long_description=open('README.md').read(),
+ )
